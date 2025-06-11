5648ee8e18e584809e2e94bbe5c4aeff31ed1627
SparklabsAcademy <sparklabsacademy@gmail.com>
2025-06-11 11:00:12 +0900

    Implement admin login and management pages

--- visionivy/visionivy.py
+++ visionivy/visionivy.py
@@ -1,4 +1,4 @@
-"""Welcome to Reflex! This file outlines the steps to create a basic app."""
+"""Simple Reflex app with admin login and management page."""
 
 import reflex as rx
 
@@ -6,33 +6,81 @@ from rxconfig import config
 
 
 class State(rx.State):
-    """The app state."""
+    """Application state handling login and consultant management."""
+
+    username: str = ""
+    password: str = ""
+    login_error: str = ""
+    new_consultant: str = ""
+    consultants: list[str] = []
+
+    def login(self) -> rx.event.EventSpec:
+        """Validate credentials and redirect if admin."""
+        if self.username == "admin" and self.password == "admin":
+            return rx.redirect("/admin")
+        self.login_error = "Invalid credentials"
+
+    def add_consultant(self) -> None:
+        """Add a consultant to the list."""
+        if self.new_consultant:
+            self.consultants.append(self.new_consultant)
+            self.new_consultant = ""
 
 
-def index() -> rx.Component:
-    # Welcome Page (Index)
-    return rx.container(
-        rx.color_mode.button(position="top-right"),
-        rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
-            rx.text(
-                "Get started by editing ",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
-                size="5",
+def login_page() -> rx.Component:
+    """Login page displayed at the index route."""
+    return rx.center(
+        rx.box(
+            rx.vstack(
+                rx.heading("Login", size="6"),
+                rx.input(
+                    placeholder="Username",
+                    on_change=State.set_username,
+                    value=State.username,
+                ),
+                rx.input(
+                    placeholder="Password",
+                    type_="password",
+                    on_change=State.set_password,
+                    value=State.password,
+                ),
+                rx.button("Sign In", on_click=State.login),
+                rx.cond(State.login_error != "", rx.text(State.login_error, color="red")),
+                spacing="4",
             ),
-            rx.link(
-                rx.button("Check out our docs!"),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
-            ),
-            spacing="5",
-            justify="center",
-            min_height="85vh",
+            padding="8",
+            border_radius="8",
+            box_shadow="md",
         ),
-        rx.logo(),
+        height="100vh",
+    )
+
+
+def admin_page() -> rx.Component:
+    """Page to manage consultant accounts after admin login."""
+    return rx.container(
+        rx.heading("Consultant Management", size="7"),
+        rx.hstack(
+            rx.input(
+                placeholder="New consultant name",
+                on_change=State.set_new_consultant,
+                value=State.new_consultant,
+            ),
+            rx.button("Add", on_click=State.add_consultant),
+        ),
+        rx.vstack(
+            rx.foreach(
+                State.consultants,
+                lambda name: rx.text(name, padding_y="1"),
+            ),
+            align_items="start",
+        ),
+        spacing="6",
+        padding_top="4",
     )
 
 
 app = rx.App()
-app.add_page(index)
+app.add_page(login_page, route="/")
+app.add_page(admin_page, route="/admin")
 
